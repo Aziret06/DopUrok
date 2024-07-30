fdfa2f7ffc83cb923d6d659d7ce4a16ca114b648
Aziret06 <aziretkarimov06@gmail.com>
2024-07-30 14:35:36 +0600

    deply to server

--- bot_config.py
+++ bot_config.py
@@ -4,7 +4,15 @@ from os import getenv
 from database.database import Database
 
 load_dotenv()
-bot = Bot(token=getenv('MY_TOKEN'))
+database = Database('db.sqlite3')
+debug = getenv('DEBUG', 0)
+if not debug:
+    print('Бот запускается на сервере')
+    from aiogram.client.session.aiohttp import AiohttpSession
+    session = AiohttpSession(proxy=getenv('PROXY'))
+    bot = Bot(token=getenv('MY_TOKEN'), session=session)
+else:
+    print('Бот запущен на компьютере')
+    bot = Bot(token=getenv('MY_TOKEN'))
 media = getenv('media')
 dp = Dispatcher()
-database = Database('db.sqlite3')
