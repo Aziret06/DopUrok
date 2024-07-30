d6113aac638cd77262ae0d0089070cb7c8aba494
Aziret06 <aziretkarimov06@gmail.com>
2024-07-30 18:04:52 +0600

    deploy

--- handlers/myinfo.py
+++ handlers/myinfo.py
@@ -6,7 +6,7 @@ myinfo_router = Router()
 
 @myinfo_router.message(Command('myinfo'))
 async def info_handler(message: types.Message):
-    print(message.from_user)
+    # print(message.from_user)
     await message.answer(f'Ваш id: {message.from_user.id}'
                          f'\nВаше имя: {message.from_user.first_name}'
                          f'\nВаш ник: {message.from_user.username}')
--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-from aiogram.filters.command import Command
-from aiogram import types, Router
 from random import choice
 import os
-from bot_config import media, bot
-from aiogram.types import FSInputFile
-
-random_recipe_router = Router()
-
+from bot_config import media
 recipe1 = ('Овсянка с ягодами:'
            '\nВ миске смешайте овсяные хлопья с молоком.'
            '\nДобавьте свежие ягоды (клубнику, малину или чернику).'
@@ -34,17 +28,8 @@ recipe5 = ('Банановые панкейки:'
 
 recipes = [recipe5, recipe1, recipe2, recipe4, recipe3]
 
-
-@random_recipe_router.message(Command('random_recipe'))
-async def recipe_handler(message: types.Message):
-    files = os.listdir('media/' + 'for_recipe')
-    random_recipe = choice(recipes)
-    ind = recipes.index(random_recipe)
-    path = 'media/' + 'for_recipe/' + files[ind]
-    print(path)
-    photo = FSInputFile(path)
-    await bot.send_photo(
-        chat_id=message.from_user.id,
-        photo=photo,
-        caption=f'{random_recipe}'
-    )
+files = os.listdir(media + 'for_recipe')
+random_recipe = choice(recipes)
+ind = recipes.index(random_recipe)
+path=media+'for_recipe'+files[ind]
+print(path)
